c130f67a9569d0b74f3983c10b01292f7856750b
Miloš Manojlović <manojlovicsmilos@hotmail.com>
2020-05-31 16:53:04 +0200

    Bubbles Expressions

--- Code/UIScenes/LoadingScene.ts
+++ Code/UIScenes/LoadingScene.ts
@@ -26,7 +26,12 @@ const USED_IMAGES = [
     "Resources/Textures/Backgrounds/A2.3.png",
     "Resources/Textures/Backgrounds/A2.4.png",
     "Resources/Textures/Backgrounds/A3.1.png",
-    "Resources/Textures/Backgrounds/A3.2.png"
+    "Resources/Textures/Backgrounds/A3.2.png",
+    "Resources/Textures/Characters/Bubbles/Sparkle.png",
+    "Resources/Textures/Characters/Bubbles/Worried.png",
+    "Resources/Textures/Characters/Bubbles/HappyClosed.png",
+    "Resources/Textures/Characters/Bubbles/HappyOpen.png",
+    "Resources/Textures/Characters/Bubbles/HappySide.png"
 ];
 
 class LoadingScene extends UIScene
